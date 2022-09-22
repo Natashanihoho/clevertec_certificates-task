f9bbeebe3408579ea5a598b5b79f3c5dd60b9355
Natasha <natashanihoho@gmail.com>
2022-09-22 12:41:27 +0200

    initial commit

---
+++ certificates-task/src/main/java/ru/clevertec/hardziyevich/CertificatesTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.hardziyevich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificatesTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificatesTaskApplication.class, args);
+	}
+
+}
